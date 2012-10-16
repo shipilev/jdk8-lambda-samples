4845ac09fbf2fce46a4aa4ab5c8d75605f538f4a
Sergey Kuksenko <walrus@walrus-laptop.(none)>
2012-10-16 19:42:28 +0400

    lambda section improved

---
+++ src/test/java/com/oracle/lambda/LambdaTest.java
@@ -0,0 +1,11 @@
+package com.oracle.lambda;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: walrus
+ * Date: 10/16/12
+ * Time: 6:45 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LambdaTest {
+}
