ef36f4b20ff0c31df2901f64633f3d835c7ca150
Aleksey Shipilev <aleksey.shipilev@gmail.com>
2012-12-12 16:17:37 +0400

    QDublin test.

---
+++ src/test/java/net/openjdk/defenders/StaticInterfaceTest.java
@@ -0,0 +1,28 @@
+package net.openjdk.defenders;
+
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class StaticInterfaceTest {
+
+    @Test
+    @Ignore // FIXME: does not run with current public builds
+    public void test() {
+        Assert.assertEquals("toDublin", TicketOffice.defaultOffice().qDublin());
+    }
+
+    public interface TicketOffice {
+        String qDublin();
+
+        static TicketOffice defaultOffice() {
+            return new TicketOffice() {
+                @Override
+                public String qDublin() {     // куда, блин?
+                    return "toDublin";        // туда, блин!
+                }
+            };
+        }
+    }
+
+}
