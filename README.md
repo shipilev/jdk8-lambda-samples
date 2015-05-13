ec1975a6990146f956f0c7d398760408c3979f32
Andrew Malyhin <am-malyhin@yandex-team.ru>
2015-05-13 23:07:09 +0300

    update to released jdk8 v40

---
+++ README.md
@@ -0,0 +1,2 @@
+## JDK8 Lambda Samples
+originally created by [@shipilev](https://github.com/shipilev), [repo](https://github.com/shipilev/jdk8-lambda-samples)
--- src/test/java/net/openjdk/lambda/FibonacciTest.java
+++ src/test/java/net/openjdk/lambda/FibonacciTest.java
@@ -9,16 +9,16 @@ import static org.junit.Assert.*;
 public class FibonacciTest {
 
     // Фибоначчийн тоог хүн төрөлхтөн өөрсдийн ахуй (source: Wikipedia MN)
-    private IntUnaryOperator fib =
-            (n) -> (n < 2) ? n : fib.applyAsInt(n - 1) + fib.applyAsInt(n - 2);
+    private IntUnaryOperator fib;
+    {fib = (n) -> (n < 2) ? n : fib.applyAsInt(n - 1) + fib.applyAsInt(n - 2);}
 
     @Test
     public void test10() {
         assertEquals(55, fib.applyAsInt(10));
     }
 
-    private static IntUnaryOperator fib_static =
-            (n) -> (n < 2) ? n : fib_static.applyAsInt(n - 1) + fib_static.applyAsInt(n - 2);
+    private static IntUnaryOperator fib_static;
+    static {fib_static = (n) -> (n < 2) ? n : fib_static.applyAsInt(n - 1) + fib_static.applyAsInt(n - 2);}
 
     @Test
     public void test10_static() {
